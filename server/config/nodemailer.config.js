acee70008d9ea7b348bd469d5d4dfa200e7f42e9
IamDevTrivedi <devtrivedi22082005@gmail.com>
2025-10-17 22:05:13 +0530

    Add nodemailer-sendgrid-transport package for email sending capabilities

--- server/config/nodemailer.config.js
+++ server/config/nodemailer.config.js
@@ -1,43 +1,30 @@
 // ./config/nodemailer.config.js
 
-
-
 // Require nodemailer
-const nodemailer = require('nodemailer');
-
+const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid-transport");
 
 // Require logger
-const logger = require('../lib/logger.lib.js');
-
+const logger = require("../lib/logger.lib.js");
 
 // Load environment variables
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT == 465,
+const transporter = nodemailer.createTransport(
+  sgTransport({
     auth: {
-        user: process.env.SMPT_USER,
-        pass: process.env.SMPT_PASSWORD,
+      api_key: process.env.SENDGRID_API_KEY,
     },
-    tls: {
-        rejectUnauthorized: false
-    },
-    connectionTimeout: 20000,
-    greetingTimeout: 20000,
-    socketTimeout: 20000
-});
-
+  })
+);
 
 transporter.verify((error, success) => {
-    if (error) {
-        logger.error('❌ Nodemailer configuration error:', error);
-    } else {
-        logger.info('✅ Nodemailer is ready to send emails');
-    }
+  if (error) {
+    logger.error("❌ Nodemailer configuration error:", error);
+  } else {
+    logger.info("✅ Nodemailer is ready to send emails");
+  }
 });
 
 module.exports = transporter;
